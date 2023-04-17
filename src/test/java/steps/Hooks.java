3bfd6b0ddd946e627d43ae69a843cecf57f2b1cb
stadnik602 <stadnik602@gmail.com>
2023-04-17 20:57:31 +0300

    Login by API test is added.

--- src/test/java/steps/Hooks.java
+++ src/test/java/steps/Hooks.java
@@ -6,7 +6,7 @@ import io.cucumber.java.en.Given;
 import static com.codeborne.selenide.Selenide.open;
 
 public class Hooks {
-    @Before
+    @Before("@UI")
     @Given("The home page is opened")
     public void openUrl() {
         open("https://grinfer.com/");
